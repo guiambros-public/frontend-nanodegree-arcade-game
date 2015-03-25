31295b4ea9070846dfdd7e2aa453789e8821b2a3
Gui Ambros <gui@wrgms.com>
2015-03-24 23:59:56 -0400

    Expanded comments.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,22 @@
 var DEBUG = false;
 
 // --------------------------
-// Enemy Class
+//  Enemy Class
 // --------------------------
+//
+// Properties
+//   . this.sprite    - relative path to enemy's sprite image
+//
+// Methods:
+//   . handleInput    - controls keyboard inputs
+//   . render         - renders player on screen
+//   . checkCollision - checks for collision between the enemy and the player object
+//   . setRandomSpeed - change enemy's speed to a random number, limited by _MAX_SPEED
+//   . setSpeed       - change enemy's speed to a specific number
+//   . setLane        - move enemy to a specific lane [1-3]
+//
 var Enemy = function(row) {
+
     // enemy properties
     this.sprite = 'images/enemy-bug.png';
 
@@ -27,7 +40,14 @@ var Enemy = function(row) {
     this.setLane(row);
 };
 
-//-- Calculate enemy's new position. Called at every tick (dt)
+
+// method Enemy.update()
+//
+//   Calculates new enemy's position.
+//
+//   This is called once at every tick (dt) by engine.js, and is independent
+//   of processor speed. The function also checks for screen wrapping on the
+//   right side, and checks for collision with player object.
 //
 Enemy.prototype.update = function(dt) {
     this._x += dt * this.speed;
@@ -37,23 +57,34 @@ Enemy.prototype.update = function(dt) {
     if (this._x > this._END_X) {
         this._x = this._START_X;
         this.setRandomSpeed();
-        if (Math.random()<0.5) {//
+        if (Math.random()<0.5) {
             this.setLane(rnd(2)+1);
         }
     }
 
+    // check for collision and increment
     if (this.checkCollision()) {
         player.reset();
         player.setScore("score-bugs", player.score_bugs += 1);
     }
 };
 
-//-- Draw the enemy on the screen
+
+// Enemy.render()
+//
+//   Renders enemy object on screen
+//
+//   This is also called once at every tick (dt) by engine.js, right after
+//   updating enemy's position through update(). The function simply draws
+//   the enemy sprite on position x,y.
+//
+//   If DEBUG variable is set to true, it'll also draw a bounding box around
+//   the enemy, so you can see and debug collision more easily.
 //
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this._x, this._y);
 
-    if (DEBUG) {    // draw the bounding box, if running in debug mode
+    if (DEBUG) {    // draw the bounding box
         ctx.beginPath();
         ctx.strokeStyle = "yellow";
         ctx.lineWidth = 3;
@@ -66,18 +97,47 @@ Enemy.prototype.render = function() {
     }
 };
 
+// Enemy.setLane(row)
+//
+//   Moves the enemy to a new lane specified by [row]. Used when enemies are
+//   respawing, so they show up on a different lane every time.
+//
 Enemy.prototype.setLane = function(row) {
     this._y = this._START_Y + (row * this._ROW_HEIGHT);
 };
 
+
+// Enemy.setRandomSpeed(row)
+//
+//   Changes enemy's speed to a random number. The max speed is controlled by
+//   constant _MAX_SPEED.
+//
 Enemy.prototype.setRandomSpeed = function() {
     this.speed = (Math.random() * this._MAX_SPEED) + (0.1 * this._MAX_SPEED);
 };
 
+
+// Enemy.setSpeed(row)
+//   Changes enemy's speed to a specific value specificed by [speed].
+//
 Enemy.prototype.setSpeed = function(speed) {
     this.speed = speed;
 };
 
+
+// Enemy.checkCollision()
+//
+//   Checks for collision between the enemy and the player object. This is
+//   achieved by executing 3 checks: //
+//      1) vertical collision, when player's centroid is between ymin/ymax
+//      2) horizontal collision, when player's centroid is between xmin/xmax
+//      3) collision with one of the corners, when distance from centroid to
+//         one of the 4 vertices of the bounding box is < player's radius
+//
+//    The Enemy shape has rounded corners, so it was used a correction
+//    factor determined by the constant [_VERTICE_TOLERANCE]. This way a
+//    collision is not prematurely flagged.
+//
 Enemy.prototype.checkCollision = function() {
     var vc = [player.centroid_x, player.centroid_y]; // player centroid
 
@@ -87,7 +147,7 @@ Enemy.prototype.checkCollision = function() {
     var v3 = [this._x + this._ENEMY_WIDTH,  this._y + this._ENEMY_TOP_PADDING + this._ENEMY_HEIGHT];
     var v4 = [this._x,                      this._y + this._ENEMY_TOP_PADDING + this._ENEMY_HEIGHT];
 
-    // check for vertical collision
+    // vertical collision
     if (vc[0] >= v1[0] && vc[0] <= v2[0]) {
         var dist = Math.min ( Math.abs(vc[1]-v1[1]),   // top collision
                               Math.abs(vc[1]-v3[1]));  // bottom collision
@@ -96,8 +156,7 @@ Enemy.prototype.checkCollision = function() {
             return true;
         }
     }
-
-    // check for horizontal collision
+    // horizontal collision
     if (vc[1] >= v2[1] && vc[1] <= v3[1]) {
         var dist = Math.min ( Math.abs(vc[0]-v1[0]),   // left collision
                               Math.abs(vc[0]-v3[0]));  // right collision
@@ -106,8 +165,7 @@ Enemy.prototype.checkCollision = function() {
             return true;
         }
     }
-
-    // check for collision with any of the 4 vertices
+    // collision with any of the 4 vertices
     dist = Math.min( dist_xy(vc, v1), dist_xy(vc, v2),
                      dist_xy(vc, v3), dist_xy(vc, v4) );
     if ((dist + this._VERTICE_TOLERANCE) <= player._RADIUS) {
@@ -117,10 +175,30 @@ Enemy.prototype.checkCollision = function() {
 };
 
 
+
+//
 // ----------------------------------
-// Player Class
+//  Player Class
 // ----------------------------------
+//
+// Properties:
+//   . centroid    - center x,y of the sprite. Used to detect collisions
+//   . pos_col/row - player position in the 5x6 map grid
+//   . score_bugs  - number of times the player died
+//   . score       - number of times player achieved the goal
+//   . sprite      - relative path to current avatar's sprite image
+//   . avatar_list - array with all possible avatars
+//
+// Methods:
+//   . handleInput  - controls keyboard inputs
+//   . render       - renders player on screen
+//   . moveTo       - move player to a new position on the 5x6 grid
+//   . changeAvatar - changes to the next available avatar
+//   . reset        - resets the player to initial position
+//   . setScore     - increments score on the page
+//
 var Player = function() {
+
     // player properties
     this.avatar_list = ['images/char-boy.png',
                         'images/char-cat-girl.png',
@@ -145,11 +223,21 @@ var Player = function() {
     this.moveTo(this.pos_col, this.pos_row);
 };
 
+
 Player.prototype.update = function(dt) {
     // not used
 };
 
-//-- Draw the player on the screen
+
+// Player.render()
+//
+//   Renders player object on screen
+//
+//   This is called once at every tick (dt) by engine.js. The function
+//   simply draws the player sprite on position x,y.
+//
+//   If DEBUG variable is set to true, it'll also draw a bounding circle
+//   around the player, so you can see and debug collision more easily.
 //
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this._x, this._y);
@@ -160,7 +248,12 @@ Player.prototype.render = function() {
     }
 };
 
-//-- Handle input arrow keys
+
+// Player.handleInput()
+//
+//   Handles keyboard input. Each corresponding arrow key moves the
+//   player in the corresponding direction. Space changes to the next
+//   available player avatar.
 //
 Player.prototype.handleInput = function(keycode) {
     debug("handling input [" + keycode + "]");
@@ -174,7 +267,18 @@ Player.prototype.handleInput = function(keycode) {
     this.moveTo(this.pos_col, this.pos_row);
 };
 
-//-- Move the player to a specific position on the grid [0-4, 0-5]
+//
+// Player.moveTo()
+//
+//   Moves the player to a specific position in the 5x6 grid.
+//
+//   The x,y coordinates are calculated using the sprite's width/height.
+//   Player's centroid is also recalculated, which is used to check
+//   for collision with enemies.
+//
+//   If player reaches to row 0 (meaning: the top-most row), the score
+//   is incremented and the player is transported back to its starting
+//   position.
 //
 Player.prototype.moveTo = function(col, row) {
     this.pos_col = col;
@@ -189,40 +293,61 @@ Player.prototype.moveTo = function(col, row) {
     }
 };
 
+
+//
+// Player.changeAvatar()
+//
+//   Iterates over the list of available avatars
+//
 Player.prototype.changeAvatar = function() {
     l = this.avatar_list.length;
     idx = this.avatar_list.indexOf(this.sprite);
     this.sprite = this.avatar_list[(idx+1)%l];
 };
 
+
+//
+// Player.reset()
+//
+//   Resets player to its starting position on the grid (2, 5)
+//
 Player.prototype.reset = function() {
     this.moveTo(2, 5);
 };
 
+
+//
+// Player.setScore()
+//
+//   Adjust score in the HTML element
+//
 Player.prototype.setScore = function(elem_id, score) {
     var e = document.getElementById(elem_id);
     e.innerHTML = score;
 };
 
 
-
 //-----------------------------------
+// Initialize all objects
 //
 
-// initialize 4 enemies, one in each lane + one randomly placed
+// 4 enemies, one in each lane, plus one randomly placed
 var allEnemies = [];
 allEnemies.push(new Enemy(1));
 allEnemies.push(new Enemy(2));
 allEnemies.push(new Enemy(3));
 allEnemies.push(new Enemy(1 + Math.floor(Math.random()*3)));
 
-// initialize player
+// create player
 var player = new Player();
 
+
+
+//
 // ------------------------------------
 // Auxiliary functions
 // ------------------------------------
-
+//
 // listen for key presses
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
